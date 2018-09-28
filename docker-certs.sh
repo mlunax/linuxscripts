8fd4bf5684043842a43ffba9b21277607f6051a2
lunax <lunaxgamingplay@gmail.com>
2018-09-28 12:02:14 +0000

    Add new file

---
+++ docker-certs.sh
@@ -0,0 +1,38 @@
+# setup
+export HOST=domain
+IP="0.0.0.0"
+
+mkdir certs
+cd certs
+
+# server certs
+openssl genrsa -aes256 -out ca-key.pem 4096
+openssl req -new -x509 -days 365 -key ca-key.pem -sha256 -out ca.pem
+openssl genrsa -out server-key.pem 4096
+openssl req -subj "/CN=$HOST" -sha256 -new -key server-key.pem -out server.csr
+echo subjectAltName = DNS:$HOST,IP:$IP,IP:127.0.0.1 >> extfile.cnf
+echo extendedKeyUsage = serverAuth >> extfile.cnf
+openssl x509 -req -days 365 -sha256 -in server.csr -CA ca.pem -CAkey ca-key.pem \
+  -CAcreateserial -out server-cert.pem -extfile extfile.cnf
+openssl genrsa -out key.pem 4096
+
+# client certs
+openssl req -subj '/CN=client' -new -key key.pem -out client.csr
+echo extendedKeyUsage = clientAuth >> extfile-client.cnf
+openssl x509 -req -days 365 -sha256 -in client.csr -CA ca.pem -CAkey ca-key.pem \
+  -CAcreateserial -out cert.pem -extfile extfile-client.cnf
+
+# cleanup
+rm -v client.csr server.csr
+
+cp ca.pem server-cert.pem server-key.pem /etc/docker/
+
+mkdir -p /etc/systemd/system/docker.service.d/
+echo '
+# /etc/systemd/system/docker.service.d/override.conf
+[Service]
+ExecStart=
+ExecStart=/usr/bin/dockerd --tlsverify --tlscacert=/etc/docker/ca.pem --tlscert=/etc/docker/server-cert.pem --tlskey=/etc/docker/server-key.pem -H fd:// -H tcp://0.0.0.0:2376' > /etc/systemd/system/docker.service.d/startup_options.conf
+
+systemctl daemon-reload
+systemctl restart docker.service
