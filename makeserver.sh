115f964c133fe843da02c970729a9180365ae4eb
lunax <lunaxgamingplay@gmail.com>
2018-09-28 11:56:15 +0000

    Update makeserver.sh

--- makeserver.sh
+++ makeserver.sh
@@ -49,8 +49,9 @@ if ( [ "$dockerbool" == "Y" ] || [ "$dockerbool" == "y" ] ) || [ -z "$dockerbool
 		echo "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | tee -a /etc/apt/sources.list.d/docker.list
 	fi
 	apt update
-	apt-get -y install docker-ce python-pip
-	pip install docker-compose
+	apt-get -y install docker-ce
+	curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+    chmod +x /usr/local/bin/docker-compose
 	systemctl enable docker
 fi
 installportainer
