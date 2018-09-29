10cdf4eb43f32d28b2efb7fa8434b9afaa81a6ec
lunax <lunaxgamingplay@gmail.com>
2018-09-29 14:33:12 +0000

    Update putSSHCert.sh

--- putSSHCert.sh
+++ putSSHCert.sh
@@ -1,3 +1,3 @@
 echo Give me the adress of ssh server
 read ADDR="root@localhost"
-cat .ssh/id_rsa.pub | ssh $ADDR 'mkdir ~/.ssh; cat >> .ssh/authorized_keys'
+cat .ssh/id_rsa.pub | ssh $ADDR 'mkdir -p ~/.ssh; cat >> .ssh/authorized_keys'
