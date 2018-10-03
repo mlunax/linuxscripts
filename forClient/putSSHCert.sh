259459b0305882e24cfc608b9a4f49c2e5786305
lunax <w.swiatek12@gmail.com>
2018-10-03 12:13:29 +0200

    Upgrade the interface of putSSHCerts.sh

--- forClient/putSSHCert.sh
+++ forClient/putSSHCert.sh
@@ -1,3 +1,9 @@
 echo Give me the adress of ssh server
 read ADDR="root@localhost"
-cat .ssh/id_rsa.pub | ssh $ADDR 'mkdir -p ~/.ssh; cat >> .ssh/authorized_keys'
+read -p	"Do you want copy certs of current user? (else from .ssh/ in this dir) [y/N]" yn
+case $yn in
+    [Yy]* ) cat ~/.ssh/id_rsa.pub | ssh $ADDR 'mkdir -p ~/.ssh; cat >> .ssh/authorized_keys';;
+    [Ny]* ) cat .ssh/id_rsa.pub | ssh $ADDR 'mkdir -p ~/.ssh; cat >> .ssh/authorized_keys';;
+	* ) cat .ssh/id_rsa.pub | ssh $ADDR 'mkdir -p ~/.ssh; cat >> .ssh/authorized_keys';;
+esac
+
