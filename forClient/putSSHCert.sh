662501e3d4a1918fa6fb0fff7a89e133418c9fb2
lunax <mlunaelumen@gmail.com>
2018-10-05 18:41:22 +0200

    fix putSSHCerts

--- forClient/putSSHCert.sh
+++ forClient/putSSHCert.sh
@@ -1,5 +1,5 @@
 echo Give me the adress of ssh server
-read ADDR="root@localhost"
+read ADDR
 read -p	"Do you want copy certs of current user? (else from .ssh/ in this dir) [y/N]" yn
 case $yn in
     [Yy]* ) cat ~/.ssh/id_rsa.pub | ssh $ADDR 'mkdir -p ~/.ssh; cat >> .ssh/authorized_keys';;
