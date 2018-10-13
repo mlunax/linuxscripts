c194d860e35e87d4f9e9c6a851c5aeff1e1eb374
lunax <mlunaelumen@gmail.com>
2018-10-13 21:07:55 +0200

    fix: first trap of bell.sh and /bin/bash at 1st line

--- fun/bell.sh
+++ fun/bell.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 me=`basename "$0"`
 echo $me "$$"
 
@@ -6,10 +7,10 @@ ctrl_c() {
 }
 for (( ; ; ))
 do
+trap ctrl_c INT
+trap ctrl_c INT
 beep -f 1000
 beep -f 2000
 beep -f 1500
-trap ctrl_c INT
-trap ctrl_c INT
 done
 
