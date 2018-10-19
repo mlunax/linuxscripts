181654abc93d401bea90c097f28d548dc1b65987
lunax <mlunaelumen@gmail.com>
2018-10-19 22:11:28 +0200

    fix: freeram fix var system and add trap

--- IDunnoWhere/freeram.sh
+++ IDunnoWhere/freeram.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
+
+ctrl_c() {
+ exit;
+}
+
 if [ ! $# == 1 ]; then
 	TIME=300;
 else
-	TIME=$1
+	TIME=$1;
 fi
+
+echo
+echo Sleep time: $TIME
+echo
+
 while true; do
-echo -------------------
-free -m;
-sync && echo 3 > /proc/sys/vm/drop_caches;
-free -m;
-echo Done
-echo -------------------
-sleep $TIME;
+	trap ctrl_c INT
+	trap ctrl_c INT
+	echo -------------------
+	free -m;
+	sync && echo 3 > /proc/sys/vm/drop_caches;
+	free -m;
+	echo Done
+	echo -------------------
+	echo
+	sleep $TIME;
 done
 
 
