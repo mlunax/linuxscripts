ffcc353f313bc7f67cdbe87c7d4b52e8167e765b
lunax <mlunaelumen@gmail.com>
2018-10-19 19:35:18 +0200

    feat: free choose of dl programs and change name

---
+++ IDunnoWhere/download_via_file.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [ ! $# == 2 ]; then
+    echo "Usage: $0 [download program] [file with download links]"
+    exit
+fi
+
+if [ ! -f $2 ]; then
+    echo "File with links not found!"
+    exit
+fi
+
+if [ $1 == "wget" ]; then
+    wget -i $2
+    exit
+else
+    $1 $(cat $2)
+    exit
+fi
+exit
