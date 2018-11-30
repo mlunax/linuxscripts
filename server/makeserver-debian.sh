2f2547bf44ad4f9dbf06b697d4de99fa334a827e
lunax <mlunaelumen@gmail.com>
2018-11-30 19:36:28 +0100

    feat: update docker-compose to 1.23.2

--- server/makeserver-debian.sh
+++ server/makeserver-debian.sh
@@ -47,7 +47,7 @@ if ( [ "$dockerbool" == "Y" ] || [ "$dockerbool" == "y" ] ) || [ -z "$dockerbool
 	fi
 	apt update
 	apt-get -y install docker-ce
-	curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+	curl -L https://github.com/docker/compose/releases/download/1.23.2/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
     chmod +x /usr/local/bin/docker-compose
 	systemctl enable docker
 fi
